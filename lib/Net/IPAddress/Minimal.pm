f97bcb1c1d9bf8bd9ddb244d043fe11076d50025
Tamir <tlousky@tlousky-pc.(none)>
2010-06-10 14:42:21 +0300

    adding module files

---
+++ lib/Net/IPAddress/Minimal.pm
@@ -0,0 +1,110 @@
+package Net::IPAddress::Minimal;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Net::IPAddress::Minimal - The great new Net::IPAddress::Minimal!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::IPAddress::Minimal;
+
+    my $foo = Net::IPAddress::Minimal->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Tamir Lousky, C<< <tlousky at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-ipaddress-minimal at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-IPAddress-Minimal>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::IPAddress::Minimal
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-IPAddress-Minimal>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-IPAddress-Minimal>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-IPAddress-Minimal>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-IPAddress-Minimal/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Tamir Lousky.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Net::IPAddress::Minimal
